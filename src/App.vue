b2a15f7f5ede339e5babc3a52f3179f1b216bf6e
Roy543 <sahilsas88@gmail.com>
2023-04-28 23:24:13 +0200

    Project initialized

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'Official VueJS Documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google.',
+          link: 'https://google.org',
+        }
+      ],
+    };
+  },
+};
+</script>
