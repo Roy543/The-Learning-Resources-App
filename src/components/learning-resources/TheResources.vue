cf48348b0928d54179d4e44c3eae26fccde5c321
Roy543 <sahilsas88@gmail.com>
2023-04-29 14:35:55 +0200

    Dynamic component added

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,29 @@
+<template>
+    <base-card>
+    <base-button @click="setselectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setselectedTab('add-resource')">Add Resource</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+
+</template>
+
+<script>
+import storedResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+    components:{
+        storedResources,
+        AddResource
+    },
+    data() {
+        return{
+            selectedTab: 'stored-resources'
+        };    
+    },
+    methods:{
+        setselectedTab(tab){
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
